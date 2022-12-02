c1fba608dd0bec02c3bf450373c7ae4dd4245bdc
Reni <reinich@outlook.com>
2022-12-02 23:39:44 +0300

    Add fix

--- 02.12.2022/Task 3 Example of string/Program.cs
+++ 02.12.2022/Task 3 Example of string/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 
 string? s = Console.ReadLine();
-Console.WriteLine(Convert.ToInt32(s[0].ToString())); // Без ToString будет конвертироваться в число в кодировке, без ToInt32 будет выводиться введенный символ, вместе сочитаются, чтобы узнать НОМЕР символа
+Console.WriteLine(Convert.ToInt32(s?[0].ToString())); // Без ToString будет конвертироваться в число в кодировке, без ToInt32 будет выводиться введенный символ, вместе сочитаются, чтобы узнать НОМЕР символа
 
 
 
