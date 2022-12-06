5a2773baa72eacbd81a23d953e5e44b708b2bbd8
Reni <reinich@outlook.com>
2022-12-06 16:41:34 +0300

    Add fix

--- 06.12.2022/Task 1 Example of string/Program.cs
+++ 06.12.2022/Task 1 Example of string/Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.Clear();
 string? n = Console.ReadLine(); // null-значение
-Console.WriteLine(Convert.ToInt32(n[0].ToString()) * 5); // char != string
+Console.WriteLine(Convert.ToInt32(n?[0].ToString()) * 5); // char != string
