f6809dfb9df8dcc0f953e61fe617a027453e2dc9
Reni <reinich@outlook.com>
2022-12-09 11:20:44 +0300

    Add seminar 09.12.2022

---
+++ 09.12.2022/Task 2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n  = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for (int i = 0; i <array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble() * (50-10) + 10,2); // [0 , 1]
+}
+Console.WriteLine($"[{string.Join(", ", array)}]"); // Джоин позволяет соединить строку, обратный сплит, [] - для красоты
