85fee892c4d6cebefab15db4a91feb70ea6e3464
Reni <reinich@outlook.com>
2022-12-16 11:32:34 +0300

    Add chenges

--- 16.12.2022/Task 3 Decimal to binary/Program.cs
+++ 16.12.2022/Task 3 Decimal to binary/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 string result = string.Empty;
-while (n>0)
+while (n > 0)
 {
     result = result + Convert.ToString(n % 2); // result = Convert.ToString(n % 2) + result; Переворот
     n /= 2;
