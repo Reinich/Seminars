f70a0be258f48d56bbc7381b72505babf49402fd
Reni <reinich@outlook.com>
2022-12-20 12:58:00 +0300

    Add 20.12.2022

---
+++ 20.12.2022/Task 1 Ex of mass w double/Program.cs
@@ -0,0 +1,38 @@
+﻿// // double n = new Random().NextDouble() * (end - begin) + begin
+// Console.WriteLine(new Random().NextDouble() * (10 + 10) - 10); // [-10, 10]
+
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (10 + 10) - 10, 2); // [-10, 10]  элементы с дробью
+}
+
+double ArrayMin(double[] array)
+{
+    double minValue = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minValue)
+            minValue = array[i];
+    }
+    return minValue;
+}
+
+double ArrayMax(double[] array)
+{
+    double maxValue = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > maxValue)
+            maxValue = array[i];
+    }
+    return maxValue;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ArrayMax(array) - ArrayMin(array)}");
