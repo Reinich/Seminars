ff41159db1d56c44a049b3882914a03af3947603
Reni <reinich@outlook.com>
2022-12-23 12:05:54 +0300

    Add 23.12.2022

---
+++ 23.12.2022/Task 1 Replace 1 and last rows/Program.cs
@@ -0,0 +1,44 @@
+﻿void OutputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+        OutputMatrix(matrix);
+    }
+}
+
+void ReplaceString(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temporary = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temporary;
+    }
+}
+
+
+
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+ReplaceString(matrix);
+Console.WriteLine();
+
+OutputMatrix(matrix);
---
+++ 23.12.2022/Task 3 How many element/Program.cs
@@ -0,0 +1,70 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, 
+// сколько раз встречается элемент входных данных.
+
+void OutputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+        OutputMatrix(matrix);
+    }
+}
+
+void DictionaryMatrix(int[,] matrix)
+{
+    int count = 1;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            count = 1;
+            if (matrix[i, j] != -100)
+            {
+                for (int k = 0; k < matrix.GetLength(0); k++)
+                {
+                    for (int m = 0; m < matrix.GetLength(1); m++)
+                    {  
+                        if (matrix[i, j] == matrix[k, m] && (i != k || j != m))
+                        {
+                            matrix[k, m] = -100;
+                            count++;
+                        }
+                        // PrintMatrix(matrix);
+                        // Console.WriteLine();
+                    }
+                }
+                Console.WriteLine($"{matrix[i, j]} встречается {count} раз");
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+DictionaryMatrix(matrix);
+Console.WriteLine();
+
+OutputMatrix(matrix);
+
+
+
+
+
+
+
+
